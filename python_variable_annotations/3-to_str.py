06dc3a5cd2e13d6145ecf70645bdc7ccc03692fe
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-08-28 15:37:26 -0300

    [3] Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""[3] Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """Type-annotated function to return the string repr of a float"""
+    return str(n)
