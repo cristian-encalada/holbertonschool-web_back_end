5a3a9038159960342652f965171961295cdea6ec
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-08-28 16:23:31 -0300

    [7] Complex types. Fix return type of Tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,6 +4,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Type-annotated function to return a string and int/float tuple"""
     return k, v*v
