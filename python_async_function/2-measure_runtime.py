acf34c302615f1a5b935acce9527ce73d655fc49
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-08-29 13:31:16 -0300

    [2] Fix. Remove max_delay default value

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n: int, max_delay: int = 10) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measures the total execution time for wait_n and returns total_time / n
     """
     start_time = time.time()
