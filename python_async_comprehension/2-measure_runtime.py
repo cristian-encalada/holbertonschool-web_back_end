77d3b636d60c68d096c378baa93d68b98ad62ef1
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-08-29 18:15:37 -0300

    [2] Fix: Add float return annotation

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """Measures the runtime of async_comprehension()
     """
     start_time = time.time()
