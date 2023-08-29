5086637320dc5d0ce1b7c93a197afc46992ffdb9
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-08-29 18:12:49 -0300

    [2] Runtime of async_comprehension()

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""[2] Run time for four parallel comprehensions
+"""
+import asyncio
+import time
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime():
+    """Measures the runtime of async_comprehension()
+    """
+    start_time = time.time()
+    # waits for all the async operations to complete
+    await asyncio.gather(async_comprehension())
+    end_time = time.time()
+    return end_time - start_time
