6c3346c69417b745281a930cf5aaa1e7fac6bc06
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-08-31 21:37:17 -0300

    [11] Fix default Union and PEP8

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -5,7 +5,7 @@ from typing import Mapping, Union, Any, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: T = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:  # noqa: E501
     """Type-annotated function using TypeVar"""
     if key in dct:
         return dct[key]
