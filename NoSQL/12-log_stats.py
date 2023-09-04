030e9220f4a1eee98b8f8aa4456cff0795441739
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-04 16:57:42 -0300

    [12] Count methods done

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,9 +6,20 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    logs = nginx_collection.count_documents({})
-    print(f'{logs} logs')
+    # first line
+    count_logs = nginx_collection.count_documents({})
+    print(f'{count_logs} logs')
 
-    status_check = nginx_collection.count_documents(
+    # second line
+    print('Methods:')
+
+    # 3rd to 7th lines
+    valid_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in valid_methods:
+        count_methods = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count_methods}')
+
+    # last line
+    count_status = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f'{status_check} status check')
+    print(f'{count_status} status check')
