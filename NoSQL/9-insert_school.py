07c7836f4ab9fe33d2eeafe5875f619979f7842c
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-04 15:05:31 -0300

    [9] Insert a document in Python

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" [9] Insert a document in Python """
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Function that inserts a new document in a collection based on kwargs"""
+    return mongo_collection.insert_one(kwargs)
--- NoSQL/README.md
+++ NoSQL/README.md
@@ -1,1 +1,7 @@
 # NoSQL
+
+# Resources
+* https://www.mongodb.com/basics
+* https://www.digitalocean.com/community/tutorials/how-to-use-aggregations-in-mongodb
+* https://realpython.com/introduction-to-mongodb-and-python/
+* https://realpython.com/introduction-to-mongodb-and-python/
