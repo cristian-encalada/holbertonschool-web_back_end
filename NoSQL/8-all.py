78110a2349ea56efdb3449ac8a4252c2808360e2
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-04 15:16:25 -0300

    [9] Fix insert_one return (inserted_id)

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """ [8] List all documents in Python
 """
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ [9] Insert a document in Python """
-from pymongo import MongoClient
 
 
 def insert_school(mongo_collection, **kwargs):
     """ Function that inserts a new document in a collection based on kwargs"""
-    return mongo_collection.insert_one(kwargs)
+    doc = mongo_collection.insert_one(kwargs)
+    return doc.inserted_id
