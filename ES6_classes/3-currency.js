1f12cf184159f4aa0a33e918ab60bd668a198e29
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-15 21:20:03 -0300

    [3] Class Currency. Add getters and setters

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -4,6 +4,26 @@ export default class Currency {
     this._name = name;
   }
 
+  // code attribute
+
+  get code() {
+    return this._code;
+  }
+
+  set code(NewCode) {
+    this._code = NewCode;
+  }
+
+  // name attribute
+
+  get name() {
+    return this._name;
+  }
+
+  set name(NewName) {
+    this._name = NewName;
+  }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
