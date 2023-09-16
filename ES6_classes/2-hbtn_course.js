04f55a5464d1c7698d2e16dd28d00195f98c49bb
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-15 21:10:11 -0300

    [2] Fix: constructor types validations

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,14 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw TypeError('Students must be an array of string');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
