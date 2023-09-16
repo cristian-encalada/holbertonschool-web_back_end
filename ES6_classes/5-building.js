a1edfc6bd0618692afe42c0657151444394d8780
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-15 21:54:49 -0300

    [5] Fix building constructor condition

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.evacuationWarningMessage === undefined) {
+    if (this.evacuationWarningMessage === undefined && this.constructor !== Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
