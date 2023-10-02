192ab628b55b7cda0b1c5159c50e75187e0ed588
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-02 19:37:09 -0300

    [1] Return a Promise, use resolve and reject

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -2,4 +2,9 @@ function getResponseFromAPI() {
   return Promise.resolve();
 }
 
+/* Another way to create a new promise (with the executor function)
+  return new Promise((resolve) => {
+  resolve(); // Resolves immediately with no value
+});
+*/
 export default getResponseFromAPI;
