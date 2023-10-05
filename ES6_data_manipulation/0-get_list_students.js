1bd005371c5e8e6ba1ee84bd5f79aff1a6137265
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-05 17:20:33 -0300

    [0] Basic list of objects, done

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+function getListStudents() {
+  // Array of student objects
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+}
+
+export default getListStudents;
