96cdc884aa0b9346c14fe3297c19a6834bb93c9f
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-10-09 12:08:08 -0300

    [8] Clean set, done

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,21 @@
+function cleanSet(set, startString) {
+  const cleanedValues = [];
+
+  // Check if startString is empty
+  if (startString === '') {
+    // Return an empty string
+    return '';
+  }
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      // If the value starts with the startString, append the rest of the string to the array
+      cleanedValues.push(value.substring(startString.length));
+    }
+  }
+
+  // Join all values with "-" and return the result
+  return cleanedValues.join('-');
+}
+
+export default cleanSet;
